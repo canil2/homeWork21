569b0bf068244e5a9e7254f8359986040dffc2b2
canil2@hotmail.com <canil2@hotmail.com>
2020-07-18 08:04:59 -0400

    Client Serving only with Prod

--- server.js
+++ server.js
@@ -52,13 +52,13 @@ app.delete("/api/books/:id", function (req, res) {
   });
 });
 
-//if (process.env.NODE_ENV == "production") {
-app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
-//}
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT || 5000, function () {
   console.log(
